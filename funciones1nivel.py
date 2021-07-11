9d66d388a3a495109dc627dd4cfbb1352f2faa3b
irena <irena.vent@gmail.com>
2021-07-11 20:24:21 +0200

    Modificado notas

--- funciones1nivel.py
+++ funciones1nivel.py
@@ -4,6 +4,9 @@ def normal (x):
 def cuadrado(x):
     return x * x
 
+def cubo (x):
+    return x**3
+
 def sumaTodos(limitTo, f):
     resultado = 0
     for i in range (limitTo+1):
@@ -11,6 +14,6 @@ def sumaTodos(limitTo, f):
     
     return (resultado)
 
-print (sumaTodos(100,normal))
+#print (sumaTodos(100,normal))
 
-print (sumaTodos (3,cuadrado))
+#print (sumaTodos (3,cuadrado))
